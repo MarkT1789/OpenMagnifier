4e21b2428639c6e928d48725babebc2c0771f355
Mark Tamura <mark_tamura@yahoo.com>
2026-08-26 12:13:26 -0700

    1.16.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "io.uglydog.magnifier"
         minSdk = 24
         targetSdk = 36
-        versionCode = 116000
-        versionName = "1.16.0"
+        versionCode = 116010
+        versionName = "1.16.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
